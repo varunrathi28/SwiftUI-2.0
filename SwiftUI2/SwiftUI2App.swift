37c0bc59cf1901ede8fe2a72373a6feb02d975ae
Varun Rathi <varunrathi28@users.noreply.github.com>
2021-05-01 23:04:07 +0530

    Start Project

---
+++ SwiftUI2/SwiftUI2App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI2App.swift
+//  SwiftUI2
+//
+//  Created by VR on 01/05/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
